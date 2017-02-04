f028c9c59db74ce9582f87c33527100ca176122b
Daniel Sarabusing <dimalantadaniel@gmail.com>
2017-02-04 11:16:28 +0800

    Front page added & more

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -20,8 +20,8 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.0",
-    "cordova-plugin-dialogs": "1.3.1"
+    "cordova-plugin-dialogs": "1.3.1",
+    "cordova-plugin-whitelist": "1.3.0"
 }
 // BOTTOM OF METADATA
 });
