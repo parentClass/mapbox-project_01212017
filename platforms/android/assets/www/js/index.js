afa7364b8d31b09e6393e36ea0354cd91ebb6595
Daniel Sarabusing <dimalantadaniel@gmail.com>
2017-02-05 16:46:15 +0800

    Project completed staging on process

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,51 +1,72 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+var routingLayer;
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function setupRoutingAPI(ghRouting,lat,lng) {
 
-        console.log('Received Event: ' + id);
-    }
-};
 
-app.initialize();
+        ghRouting.addPoint(new GHInput(lat,lng));
+
+        if (ghRouting.points.length > 1) {
+            // ******************
+            //  Calculate route! 
+            // ******************
+            ghRouting.doRequest(function (json) {
+                if (json.message) {
+                    var str = "An error occured: " + json.message;
+                    if (json.hints)
+                        str += json.hints;
+                        console.log(str);
+
+                } else {
+                    var path = json.paths[0];
+                    routingLayer.addData({
+                        "type": "Feature",
+                        "geometry": path.points
+                    });
+
+                    // var outHtml = "Distance in meter:" + path.distance;
+                    // outHtml += "<br/>Times in seconds:" + path.time / 1000;
+                    // outHtml += "<br/><a href='" + ghRouting.getGraphHopperMapsLink() + "'>GraphHopper Maps</a>";
+                    // $("#routing-response").html(outHtml);
+
+                    // if (path.bbox) {
+                    //     var minLon = path.bbox[0];
+                    //     var minLat = path.bbox[1];
+                    //     var maxLon = path.bbox[2];
+                    //     var maxLat = path.bbox[3];
+                    //     var tmpB = new L.LatLngBounds(new L.LatLng(minLat, minLon), new L.LatLng(maxLat, maxLon));
+                    //     map.fitBounds(tmpB);
+                    // }
+
+                    // instructionsDiv.empty();
+                    // if (path.instructions) {
+                    //     var allPoints = path.points.coordinates;
+                    //     var listUL = $("<ol>");
+                    //     instructionsDiv.append(listUL);
+                    //     for (var idx in path.instructions) {
+                    //         var instr = path.instructions[idx];
+
+                    //         // use 'interval' to find the geometry (list of points) until the next instruction
+                    //         var instruction_points = allPoints.slice(instr.interval[0], instr.interval[1]);
+
+                    //         // use 'sign' to display e.g. equally named images
+
+                    //         $("<li>" + instr.text + " <small>(" + ghRouting.getTurnText(instr.sign) + ")</small>"
+                    //                 + " for " + instr.distance + "m and " + Math.round(instr.time / 1000) + "sec"
+                    //                 + ", geometry points:" + instruction_points.length + "</li>").
+                    //                 appendTo(listUL);
+                    //     }
+                    // }
+                }
+            });
+        }
+
+    routingLayer = L.geoJson().addTo(map);
+    routingLayer.options = {
+        style: {color: "#9C27B0", "weight": 5, "opacity": 0.6}
+    };
+}
+
+function clearRoutingLayer(ghRouting){
+    this.routingLayer.clearLayers();
+    ghRouting.clearPoints();
+}
